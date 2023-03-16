395b967af95dd044af93f9e93c63a8b8f4eeb54d
Connor Mclaughlin <connormclaughlin@Connors-MacBook-Pro.local>
2023-03-16 02:51:29 -0700

    dashboard

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
 var city = $("#cityinput")
+var searchBtn = $('#searchbtn')
+var input = document.querySelector('cityinput')
 var apiKey = "0e58904f442705bd2a5dbc3d0662d360"
 var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+const fiveDayContainer = document.querySelector("#forecast")
+function getAPI(queryURL) {
+  fetch(queryURL)
+}
 
-function getAPI(queryURL) { fetch(queryURL) }
 
 
-
-
-var searchBtn = $('#search-button')
-searchBtn.on('click'), searchWeather
 function searchWeather(event) {
   console.log(event.currentTarget)
   var clickSave = event.currentTarget;
   var textBox = $(clickSave).siblings("input");
   var textToSave = textBox.val()
   var cityName = $('<li></li>').text(textToSave)
-  
+
   $('#city-name').text(textToSave)
   $('#history').prepend(cityName)
   $('#cityinput').val('');
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + "textToSave+&appid=0e58904f442705bd2a5dbc3d0662d360").then(
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + textToSave + "&appid=0e58904f442705bd2a5dbc3d0662d360").then(
     function (response) {
       return response.json()
+
     }
   )
     .then(function (data) {
-      var temp = data.main.temp
-      var tempF = (Math.trunc((temp - 273.15) * 9 / 5 + 32))
-      $("#temp").text(tempF + " °F");
-      var tempL = data.main.temp_min
       console.log(data)
-      var tempLF = (Math.trunc((tempL - 273.15) * 9 / 5 + 32))
-      $("#tempL").text(tempLF + " °F");
-
-      var tempM = data.main.temp_max
-      var tempMF = (Math.trunc((tempM - 273.15) * 9 / 5 + 32))
-      $("#tempM").text(tempMF + " °F");
       if (data.clouds.all > 75) {
         $("#city-name").append(" ☁️")
       } else if (data.clouds.all < 15) {
@@ -43,8 +35,67 @@ function searchWeather(event) {
       } else {
         $("#city-name").append(" ⛅")
       }
-      var humidity = data.main.humidity
-      $("#Humidity").text(humidity + "%")
+      var temp = data.main.temp
 
+      var tempF = (Math.trunc((temp - 273.15) * 9 / 5 + 32))
+      $("#current").text(tempF + " °F");
+      console.log(tempF)
+
+      var tempL = data.main.temp_min
+
+      var tempLF = (Math.trunc((tempL - 273.15) * 9 / 5 + 32))
+      $("#tempL").text(tempLF + " °F");
+      console.log(tempLF)
+
+      var tempH = data.main.temp_max
+      var tempHF = (Math.trunc((tempH - 273.15) * 9 / 5 + 32))
+      $("#tempH").text(tempHF + " °F");
+      console.log(tempHF)
+
+
+      var humidity = data.main.humidity
+      $("#humid").text(humidity + "%")
+      var lat = data.coord.lat;
+      var lon = data.coord.lon
+      getForecast(lat, lon)
     })
 }
+
+const getForecast = function (lat, lon) {
+  const forecastArray = []
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    .then(function (response) {
+      return response.json()
+    })
+    // console.log(response)
+    .then(function (data) {
+      console.log(data)
+      console.log(data.cnt)
+      for (let i = 0; i < data.cnt; i += 8) {
+        console.log(data)
+        console.log(forecastArray)
+        const forecastCard =
+          `<div class="block">   
+                   <div class="content">
+                       <p id="forecast-date">${data.list[i].dt_txt.split(" ")[0]}</p>
+                       <p id="weather-5-day">${data.list[i].weather[0].description}</p>
+                       <p id="temp-5-day">Temp:${data.list[i].main.temp}</p>
+                       <p id="wind-5-day">Wind:${data.list[i].wind.speed}t</p>
+                       <p id="humidity-5-day">Humidity:${data.list[i].main.humidity}</p>
+                 </div>
+              </div>`
+
+        forecastArray.push(forecastCard)
+
+      } fiveDayContainer.innerHTML = forecastArray
+    })
+
+}
+
+
+// searchBtn.on("click", function(event){
+//   event.preventDefault()
+//   searchWeather
+//   getForecast(city.value)
+// });
+searchBtn.on('click', searchWeather)
